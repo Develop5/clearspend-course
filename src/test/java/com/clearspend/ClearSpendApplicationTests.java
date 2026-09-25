d23f630194a2512ee2bedd1b2a8c97d5e86277ee
Develop5 <38910436+Develop5@users.noreply.github.com>
2026-09-25 18:52:33 +0200

    Build ClearSpend day1-start

---
+++ src/test/java/com/clearspend/ClearSpendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.clearspend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSpendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
