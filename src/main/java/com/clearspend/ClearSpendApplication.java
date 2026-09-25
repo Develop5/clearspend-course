ade726ab3fbd4dd5e182e6779d5657d76dd17e87
Develop5 <38910436+Develop5@users.noreply.github.com>
2026-09-25 18:51:50 +0200

    Build ClearSpend day1-start

---
+++ src/main/java/com/clearspend/ClearSpendApplication.java
@@ -0,0 +1,12 @@
+package com.clearspend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearSpendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearSpendApplication.class, args);
+    }
+}
